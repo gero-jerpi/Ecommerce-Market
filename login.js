9b90232eca9fa76e26785385ece78c528b340dd3
gero-jerpi <gero_jerpi@hotmail.com>
2023-06-11 21:49:31 -0300

    Arreglos en el login y register

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 import './style.css'
-import { storedUsers } from './register'
+import { storedUsers } from './register.js'
 
 // Login
 
@@ -7,14 +7,21 @@ const userLogin = document.getElementById("user-login")
 const passwordLogin = document.getElementById("password-login")
 const formLogin = document.getElementById("form-login")
 
+console.log(storedUsers);
+
 formLogin.addEventListener("submit", (e) => {
     e.preventDefault()
-    const verifyUser = storedUsers.find((user)=>user.user === userLogin.value && user.password === passwordLogin.value)
+    const verifyUser = storedUsers.find((user) => user.user === userLogin.value && user.password === passwordLogin.value)
     console.log(verifyUser);
-    if (verifyUser != undefined) {
-        alert("Usuario logeado")
-    }else{
-        alert("Datos incorrectos")
+    if (verifyUser === storedUsers[0]) {
+        alert("admin logeado")
+        document.location.href = "admin.html"
+
+    } else if (verifyUser != storedUsers[0] && verifyUser != undefined) {
+        alert("usuario logeado")
+        document.location.href = "index.html"
+    } else {
+        alert("datos incorrectos")
     }
 })
 
--- register.js
+++ register.js
@@ -6,17 +6,42 @@ const userRegister = document.getElementById("user-register")
 const passwordRegister = document.getElementById("password-register")
 const formRegister = document.getElementById("form-register")
 
-export const storedUsers = []
+export let storedUsers = [{
+    user: "admin",
+    password: "123"
+}]
 
 formRegister.addEventListener("submit", (e) => {
     e.preventDefault()
-    let createdUser = {
-        user: userRegister.value,
-        password: passwordRegister.value
+    const userSearch = storedUsers.find((user) => user.user === userRegister.value)
+    if (userSearch === undefined) {
+        let createdUser = {
+            user: userRegister.value,
+            password: passwordRegister.value
+        }
+        storedUsers.push(createdUser)
+
+        localStorage.setItem("users", JSON.stringify(storedUsers))
+
+        console.log(storedUsers);
+
+        alert("Usuario creado")
+        // Redirección: document.location.href = "login.html"
+    } else {
+        alert("Usuario ya existente")
     }
-    storedUsers.push(createdUser)
-    console.log(storedUsers);
+
 })
+
+function verifyLocalStorage(){
+    const localStorageUsers = JSON.parse(localStorage.getItem("users"))
+    storedUsers = localStorageUsers
+    console.log("Usuarios almacenados localmente", storedUsers);
+}
+verifyLocalStorage()
+
+
+
 
 
 
